50de93fb7304a69db972d477a879765ff8db77c1
putiXixi <admin@putileaf.top>
2024-11-30 19:10:31 +0800

    yam更新

--- 视角.h
+++ 视角.h
@@ -1,13 +1,11 @@
 #pragma once
 #include <cstddef>
 
-<<<<<<< HEAD
+
+
 namespace ÊÓ½Ç {
 	constexpr std::ptrdiff_t pitch = 0x1A5E650;
-=======
-namespace è§†è§’ {
-	constexpr std::ptrdiff_t pitch = 0x1A5C610;
->>>>>>> 994091cfa0b709dcea65d5f1d87ba79862ae87ce
+
 	constexpr std::ptrdiff_t yam = pitch+4;
 
 }
