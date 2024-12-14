307c5fbce6535ae0a67a6b9d15593e1619d3cea4
4eloveksc <58350394+FaHaT-TM@users.noreply.github.com>
2024-12-14 21:14:52 +0700

    Update cheat.h

--- cheat.h
+++ cheat.h
@@ -1,4 +1,4 @@
-﻿#pragma once
+#pragma once
 #include"工具类/Mem.h"
 #include"myimgui.h"
 #include "Menu.h"
@@ -9,82 +9,81 @@
 #include"视角.h"
 
 #include<cmath>
-#include <iostream>
+#include<iostream>
 #include<vector>
 
-
-
-struct D2D
-{
-	float x, y;
-};
-struct D3D
-{
-	float x, y, z;
+// 2D point structure
+struct D2D {
+    float x, y;
 };
 
+// 3D point structure 
+struct D3D {
+    float x, y, z;
+};
 
-//取准星距离
-float calculateDistance(const D2D 准星, const D2D 对象);
-
+// Calculate distance between crosshair and target
+float calculateDistance(const D2D crosshair, const D2D target);
 
 namespace cheat {
 
-	inline char* EnterAimAddress[2];
-	inline float ActorDistance[2] = { 99999.f,999999.f};
+    // Aim addresses
+    inline char* EnterAimAddress[2];
+    inline float ActorDistance[2] = { 99999.f, 999999.f };
 
+    // Screen dimensions
+    inline float screenWidth;
+    inline float screenHeight;
 
-	inline float 屏幕宽度;
-	inline float 屏幕高度;
+    // Window and process info
+    inline HWND g_hwnd;
+    inline char* clientAddress;    // Module address
+    inline DWORD g_pid;           // Process ID
+    inline HANDLE g_handle;       // Process handle
+    inline ImVec2 g_size;
 
-	inline HWND g_hwnd;
-	//模块地址
-	inline char* clientAddress;
-	//pid
-	inline DWORD g_pid;
-	//进程句柄
-	inline HANDLE g_handle;
-	inline ImVec2 g_size;
+    inline float Matrix[4][4];    // View matrix
+    inline char* AimAddress[2];   
+    inline float FOV;             // Aimbot FOV
+    inline D2D Aimmouse;
 
-	inline float Matrix[4][4]; //矩阵
-	inline char* AimAddress[2];
-	inline float FOV;//自瞄范围
-	inline D2D Aimmouse;
-	
+    // Player information structure
+    struct ActorInfo {
+        char* Address[2];         // Base address
+        char* SkeletonAddress[2]; // Skeleton address
+        int Health;               // Health points
+        int camp;                 // Team ID
+        D3D Axis;                // World position
+        float x1;
+        float y1; 
+        float x2;
+        float y2;
+        float distance;
+    };
 
-	struct ActorInfo
-	{
-		char* Address[2]; 	//基础地址
-		char* SkeletonAddress[2]; //骨骼地址
-		int Health; //血量
-		int camp;	 //阵营
-		D3D Axis; //坐标
-		float x1;
-		float y1;
-		float x2;
-		float y2;
-		float 距离;
-	};
-	inline ActorInfo LocalPlayer; //本地
-	inline ActorInfo ActorPlayer; //对象玩家
-	
-	inline std::vector<ActorInfo> targets; // 存储当前可以瞄准的目标
-	
+    inline ActorInfo LocalPlayer;              // Local player info
+    inline ActorInfo ActorPlayer;              // Target player info
+    inline std::vector<ActorInfo> targets;     // Valid aimbot targets
 }
 
+// Core functions
 void fun();
 void Traverse();
-//读取本地玩家Pawn
-bool ReadLocalPawn();
-//读取对象玩家Pawn
-bool ReadActorPawn();
-//三维转二维
+
+// Player data reading
+bool ReadLocalPawn();    // Read local player pawn
+bool ReadActorPawn();    // Read target player pawn
+
+// Coordinate conversion
 bool WorldScreen2d(_In_ float world[3], _Out_ float screen[2]);
 
-bool 绘制测试();
-void 绘制血条();
-void 绘制骨骼();
-void 骨骼连线(char* Address, int start, int End);
+// Drawing functions
+bool DrawTest();
+void DrawHealthBar(); 
+void DrawSkeleton();
+void DrawBoneLine(char* Address, int start, int End);
 bool The2DBoxsize();
-void 自瞄();
+
+// Aimbot functions
+void Aimbot();
 D2D Aiming(D3D LocalAxis, char* AimAddress);
